d8ca95bf9c06d58f4092d514b2cd502b8f806dff
Eduardo Simões <123998309+eduardo895@users.noreply.github.com>
2025-10-14 20:11:35 +0000

    Adicionado formulário para criar entries no cofre

--- renderer.js
+++ renderer.js
@@ -1,16 +1,30 @@
 const { loadVault, saveVault } = require('./src/vault');
 let masterPassword;
+let currentVault;
 
 document.getElementById('openVault').addEventListener('click', () => {
-    masterPassword = prompt('Digite a password mestra: ');
-    try{
-        const vault = loadVault(masterPassword);
-        console.log('Vault carregado', vault);
-        alert('Vault aberto - vê a consola para detalhes.')
-    } catch {
-        const emptyVault = { entries: [] };
-        saveVault(emptyVault, masterPassword);
-        alert('Novo vault criado!');
-        console.log('Cofre inicializado:', emptyVault);
-    }
+  masterPassword = prompt('Digite a password mestra:');
+  try {
+    currentVault = loadVault(masterPassword);
+    alert('Cofre carregado!');
+  } catch {
+    currentVault = { entries: [] };
+    saveVault(currentVault, masterPassword);
+    alert('Novo cofre criado!');
+  }
+});
+
+document.getElementById('entryForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const entry = {
+    title:    document.getElementById('title').value,
+    username: document.getElementById('username').value,
+    password: document.getElementById('password').value,
+    url:      document.getElementById('url').value,
+    notes:    document.getElementById('notes').value
+  };
+  currentVault.entries.push(entry);
+  saveVault(currentVault, masterPassword);
+  alert('Entrada adicionada com sucesso!');
+  e.target.reset();
 });
