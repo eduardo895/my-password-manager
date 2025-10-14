cc6cc81e94f599136e9a31a3ec127e6266dbf353
Eduardo Simões <123998309+eduardo895@users.noreply.github.com>
2025-10-14 18:16:36 +0000

    Estrutura base com Electron e interface inicial.

---
+++ renderer.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('openVault');
+    btn.addEventListener('click', () => {
+        alert("Ainda por implementar!")
+    });
+})
