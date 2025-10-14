7e098cda75c8fffc3c68215e54a08a4e36348915
Eduardo Simões <123998309+eduardo895@users.noreply.github.com>
2025-10-14 19:27:07 +0000

    Ligado a interface ao módulo de vault (load/save)

--- renderer.js
+++ renderer.js
@@ -1,6 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('openVault');
-    btn.addEventListener('click', () => {
-        alert("Ainda por implementar!")
-    });
-})
+const { loadVault, saveVault } = require('./src/vault');
+let masterPassword;
+
+document.getElementById('openVault').addEventListener('click', () => {
+    masterPassword = prompt('Digite a password mestra: ');
+    try{
+        const vault = loadVault(masterPassword);
+        console.log('Vault carregado', vault);
+        alert('Vault aberto - vê a consola para detalhes.')
+    } catch {
+        const emptyVault = { entries: [] };
+        saveVault(emptyVault, masterPassword);
+        alert('Novo vault criado!');
+        console.log('Cofre inicializado:', emptyVault);
+    }
+});
